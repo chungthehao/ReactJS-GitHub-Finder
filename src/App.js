7160a040f0525817cbe2d50a34506c774b550e9a
Henry Chung <chungthehao@gmail.com>
2019-07-11 15:03:05 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    // # Without JSX
+    // return React.createElement(
+    //   'div',
+    //   { className: 'App' },
+    //   React.createElement('h1', null, 'Hi Everyone!!')
+    // );
+
+    return (
+      <div className='App'>
+        <h1>Hi Everyone!</h1>
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className='App'>
+//       <h1>Hi Everyone!</h1>
+//     </div>
+//   );
+// }
 
 export default App;
